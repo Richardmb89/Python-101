e8cce67a6452b46475987485a29bae612ded608d
Richardmb89 <34818869+Richardmb89@users.noreply.github.com>
2019-07-21 00:13:25 -0400

    Update FireCode Count The Leaves.py

--- FireCode Count The Leaves.py
+++ FireCode Count The Leaves.py
@@ -1,4 +1,16 @@
 class BinaryTree:
+    #Write a function to find the total number of leaf nodes in a binary tree.
+    #A node is described as a leaf node if it doesn't have any children. If there are no leaf nodes, return 0.
+
+#    Example:
+ #      1
+  #    / \
+   #  2   3     
+ #   / \ / \
+ #  4  5 6  7
+ # / \
+# 8   9     
+# ==> no. of leaves = 5
     def __init__(self, root_node = None):
             self.root = root_node
 
